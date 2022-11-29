c53a7c935a5c1f52d75901b961d04e10aafcf62b
Romagnhp <romagnhp@gmail.com>
2022-11-29 13:06:57 +0200

    edit print out for class

--- 3_main.py
+++ 3_main.py
@@ -5,23 +5,31 @@ class Point:
         self.pointX = pointX
         self.pointY = pointY
     def __str__(self) -> str:
-        return f'left top angle is - ({self.pointX}, {self.pointY})'
+        return f'Верхняя левая точка фигуры - ({self.pointX}, {self.pointY})'
 
 class Shape(Point):
-    def __init__(self,__objPoint, pointX = 0, pointY = 0,) -> None:
+    def __init__(self,__objPoint, width, height, pointX = 0, pointY = 0,) -> None:
         super().__init__(pointX, pointY)
         self.__leftTopAngle = __objPoint
+        self.dimensionHeight = height
+        self.dimensionWidth = width
+        
+       
         
     # сохранение фигуры в файл
     def Save(self):
         with open('shape.dll', mode='wb') as file:
             pickle.dump(self.__leftTopAngle, file)
+            pickle.dump(self.dimensionWidth, file)
+            pickle.dump(self.dimensionHeight, file)
 
     # считывание фигуры из файла
     def Load(self):
         with open ('shape.dll', mode='rb') as file:
-            temp = pickle.load(file)
-        return temp
+            temp1 = pickle.load(file)
+            temp2 = pickle.load(file)
+            temp3 = pickle.load(file)
+        return f'{temp1},\nгабаритный размер вдоль оси Х - {temp2},\nгабаритный размер вдоль оси Y - {temp3}'
 
     # вывод на экран информации о фигуре
     def Show(self):
@@ -29,31 +37,27 @@ class Shape(Point):
         print(temp)
 
 class Rectangle(Shape):
-    def __init__(self,__objPoint, width, higth, pointX = 0, pointY = 0,) -> None:
-        super().__init__(__objPoint, pointX, pointY)
-        self.__width = width
-        self.__higth = higth
+        def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
+            super().__init__(__objPoint, width, height, pointX, pointY)
+            print("\nфигура  - прямоугольник")
 
-class Square(Rectangle):
+class Square(Shape):
+    def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
+        super().__init__(__objPoint, width, height, pointX, pointY)        
+        print("фигура квадрат")
 
-    def __init__(self, __objPoint, width, higth, pointX = 0, pointY = 0) -> None:
-        super().__init__(__objPoint, width, higth, pointX, pointY)
+class Circle(Shape):
+    def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
+        super().__init__(__objPoint, width, height, pointX, pointY)
+        print('фигура окружность')
 
-      
-class Circle(Rectangle):
-
-    def __init__(self, __objPoint, width, higth, pointX = 0, pointY = 0) -> None:
-        super().__init__(__objPoint, width, higth, pointX, pointY)
-
-
-class Ellips(Rectangle):
-
-    def __init__(self, __objPoint, width, higth, pointX = 0, pointY = 0) -> None:
-        super().__init__(__objPoint, width, higth, pointX, pointY)
-    pass
+class Ellips(Shape):
+    def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
+        super().__init__(__objPoint, width, height, pointX, pointY)
+        print('фигура эллипс')
 
 point_1 = Point(4,5)
-shape_1 = Shape(point_1)
+shape_1 = Shape(point_1, 100, 120)
 
 shape_1.Save()
 shape_1.Load()
