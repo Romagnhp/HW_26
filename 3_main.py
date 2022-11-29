32d457d38bb734a65ebb8f8c60dc5dd7063e88db
Romagnhp <romagnhp@gmail.com>
2022-11-29 14:26:53 +0200

    for class Circl output  parametrs

--- 3_main.py
+++ 3_main.py
@@ -4,8 +4,12 @@ class Point:
     def __init__(self, pointX, pointY) -> None:
         self.pointX = pointX
         self.pointY = pointY
+
+    def __add__(self, __objPoint):
+        return Point(self.pointX + __objPoint.pointX, self.pointY + __objPoint.pointY)
+
     def __str__(self) -> str:
-        return f'Верхняя левая точка фигуры - ({self.pointX}, {self.pointY})'
+        return f'({self.pointX}, {self.pointY})'
 
 class Shape(Point):
     def __init__(self,__objPoint, width, height, pointX = 0, pointY = 0,) -> None:
@@ -13,8 +17,10 @@ class Shape(Point):
         self.__leftTopAngle = __objPoint
         self.dimensionHeight = height
         self.dimensionWidth = width
-        
-       
+
+    # метод для доступв к левой верхней тчк в дочерних классах
+    def getleftTopAngle(self):
+        return self.__leftTopAngle
         
     # сохранение фигуры в файл
     def Save(self):
@@ -29,7 +35,7 @@ class Shape(Point):
             temp1 = pickle.load(file)
             temp2 = pickle.load(file)
             temp3 = pickle.load(file)
-        return f'{temp1},\nгабаритный размер вдоль оси Х - {temp2},\nгабаритный размер вдоль оси Y - {temp3}'
+        return f'Верхняя левая точка фигуры - {temp1},\nгабаритный размер вдоль оси Х - {temp2},\nгабаритный размер вдоль оси Y - {temp3}'
 
     # вывод на экран информации о фигуре
     def Show(self):
@@ -44,28 +50,53 @@ class Rectangle(Shape):
 class Square(Shape):
     def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
         super().__init__(__objPoint, width, height, pointX, pointY)        
-        print("фигура квадрат")
+        print("\nфигура квадрат")
 
 class Circle(Shape):
     def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
         super().__init__(__objPoint, width, height, pointX, pointY)
-        print('фигура окружность')
+        print('\nфигура окружность')
+
+    def defindPointCenter(self):
+        # создание абстрактой тчк.
+        # width == height 
+
+        somePoint = Point(self.dimensionWidth/2, self.dimensionHeight/2)
+        
+        # доступ к верхней левой тчк. через метод дочернего класса Shape
+        LeftTopAngle = super().getleftTopAngle()
+
+        # использование перегруженного оператора '+' класса Point
+        centerCircl = LeftTopAngle + somePoint
+        return centerCircl
+
+    def __str__(self) -> str:
+        return f'координаты центра окружности - {self.defindPointCenter()}, радиус - {self.dimensionHeight/2}'
 
 class Ellips(Shape):
     def __init__(self, __objPoint, width, height, pointX=0, pointY=0) -> None:
         super().__init__(__objPoint, width, height, pointX, pointY)
-        print('фигура эллипс')
+        print('\nфигура эллипс')
+
+
+# ПРОВЕРКА
 
 point_1 = Point(4,5)
-shape_1 = Shape(point_1, 100, 120)
+shape_1 = Shape(point_1, 0, 0)
 
 shape_1.Save()
 shape_1.Load()
 shape_1.Show()
-
 
 point_2 = Point(6,8)
 rectangle_1 = Rectangle(point_2, 10, 50)
 rectangle_1.Save()
 rectangle_1.Load()
 rectangle_1.Show()
+
+point_3 = Point(10, 12)
+circle_1 = Circle(point_2, 60, 60)
+circle_1.Save()
+circle_1.Load()
+circle_1.Show()
+print(circle_1)
